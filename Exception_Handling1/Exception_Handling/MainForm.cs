3751cd483302334fc75162b1424e9d459d9471f6
Samuel Tollefson <sam8879@gmail.com>
2015-05-07 00:25:07 -0400

    .NET2

---
+++ Exception_Handling1/Exception_Handling/MainForm.cs
@@ -0,0 +1,125 @@
+﻿// Exception Handling
+// Samuel Tollefson
+// POS/409
+// April 20, 2015
+// Jon Jensen
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exception_Handling
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int rowIndex = 0;
+
+            try
+            {
+                // Check for null data.
+                if (txtID.Text == "" | txtFName.Text == "" | txtLName.Text == "" | txtPhone.Text == "")
+                {
+                    throw new ArgumentNullException();
+                }
+
+                // Check current list for duplicate ID.
+                foreach (DataGridViewRow row in dgvPeople.Rows)
+                {
+                    if (txtID.Text == dgvPeople.Rows[rowIndex].Cells[0].Value.ToString())
+                    {
+                        throw new IDAlreadyExistsException();
+                    }
+                    rowIndex++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            // Used for following ID check
+            char[] b = new char[6]; 
+            List<string> idCollectionString = Person.searchList().Split(',').ToList();
+
+            // Check text file for duplicate ID.
+            foreach (string s in idCollectionString)
+            {
+                try
+                {
+                    using (StringReader sr = new StringReader(s))
+                    {
+                        sr.Read(b, 0, 6);
+                        string str = new string(b);
+
+                        if (txtID.Text == str)
+                        {
+                            throw new IDAlreadyExistsException();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+
+            // Set the variables.
+            new Person(txtID.Text, txtFName.Text, txtLName.Text, txtPhone.Text);
+
+            // Display the new person.
+            dgvPeople.Rows.Add(Person.PersonID, Person.FName, Person.LName, Person.PhoneNumber);
+
+            Person.addPersonToList();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearTextBoxes()
+        {
+            Action<Control.ControlCollection> func = null;
+
+            func = (controls) =>
+            {
+                foreach (Control control in controls)
+                    if (control is TextBox)
+                        (control as TextBox).Clear();
+                    else
+                        func(control.Controls);
+            };
+
+            func(Controls);
+        }
+
+        private void btnAddToFile_Click(object sender, EventArgs e)
+        {
+            Person.addListToFile();
+
+            MessageBox.Show("List has been added to file.");
+        }
+    }
+}
---
+++ Exception_Handling1/Exception_Handling/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exception_Handling
+{
+    class Person
+    {
+        public static string PersonID { get; set; }
+        public static string FName { get; set; }
+        public static string LName { get; set; }
+        public static string PhoneNumber { get; set; }
+
+        public static List<string> people = new List<string>();
+
+        private static string path = Application.StartupPath + "//People.txt";
+
+        public Person(string id, string first, string last, string phone)
+        {
+            PersonID = id;
+            FName = first;
+            LName = last;
+            PhoneNumber = phone;
+        }
+
+        public static void addPersonToList()
+        {
+            people.Add(PersonID + " " + FName + " " + LName + " " + PhoneNumber);
+        }
+
+        public static void addListToFile()
+        {
+            try
+            {
+                //Write to file.
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    foreach (var item in people)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Method used to check text file for duplicate ID.
+        public static string searchList()
+        {
+            List<string> lines = new List<string>();
+
+            List<string> idCollection = new List<string>();
+
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+                foreach (string s in lines)
+                {
+                    idCollection.Add(s);
+                }
+            }
+            catch (Exception)
+            {
+                var myFile = File.Create(path);
+                myFile.Close();
+            }
+            string idCollectionString = String.Join(",", idCollection);
+            return idCollectionString;
+        }
+    }
+}
